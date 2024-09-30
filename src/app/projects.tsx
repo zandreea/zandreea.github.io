55b2ea21b9e4831f4211245bf9ca4c8e99c94ccf
Andreea Zlei <zleiandreea@gmail.com>
2024-09-30 19:19:14 +0200

    fix path

---
+++ src/app/projects.tsx
@@ -0,0 +1,44 @@
+const projects = [
+    {
+      title: "Research Paper on Teaching ML",
+      description: "Conducted research on machine learning education, analyzing teaching methods through a literature review, a survey of ML lecturers in Dutch universities, and a comparative experiment to assess the effectiveness of traditional methods in equipping future engineers with essential ML skills.",
+      image: "/images/ML-poster.png",
+      link: "https://repository.tudelft.nl/record/uuid:f5320370-68dc-4da8-8c96-d08ee54b9446",
+      tags: ["Machine Learning", "Teaching Methodologies", "Python"]
+    },
+    {
+      title: "App that Uses Git to Treat Academic Outputs as Software Artifacts",
+      description: "Co-developed an open-source platform called Alexandria, designed to rethink scientific publishing by drawing on best practices from software engineering. By using git versioning, the platform enables transparent collaboration, continuous improvement, and open peer review of academic outputs.",
+      image: "/images/alexandria.png",
+      link: "https://github.com/prjct-alexandria/Alexandria",
+      tags: ["React", "Typescript", "Go", "Git"]
+    },
+    {
+      title: "C++ Project on Hounds and Fox Game with AI Player",
+      description: "Co-implemented the game Hounds and Foxes, with an AI player that uses several strategies in a smart way in order to be unbeatable.",
+      image: "/images/dogs-and-fox.jpg",
+      link: "https://github.com/paulsem/dogsandfox1",
+      tags: ["C++", "Artificial Intelligence"]
+    },
+    {
+      title: "C# NASA Hackathon Game on Space Exploration using Hubble Data",
+      description: "Co-created a game using images from the Hubble Space Telescope as integral components. Hubbletale is a browser-based game that has an exploratory, action-packed approach to storytelling.",
+      image: "/images/hubbleTale.png",
+      link: "",
+      tags: ["C#", "Hubble telescope data"]
+    },
+    {
+      title: "Java App on Managing Reservations on Campus",
+      description: "Co-developed a campus reservation application in Java, in which you could book rooms, view your calendar, order food, and more.",
+      image: "/images/campus-manager.png",
+      link: "",
+      tags: ["Java", "JavaFX"]
+    },
+    {
+      title: "Discord Bot with Tic Tac Toe and Mastermind",
+      description: "Developed this bot for personal use in a discord server, with discord.js, a node.js module. Implemented a single player Mastermind game and a two-player Tic-TacToe game, along with some basic bot commands. Made games playable by users in the server by calling specific commands, and designed a simple, yet effective interface by printing ASCII art in the chat.",
+      image: "/images/discord.webp",
+      link: "https://github.com/zandreea/discord-js-bot",
+      tags: ["NodeJs", "DiscordJs"]
+    },
+  ];
